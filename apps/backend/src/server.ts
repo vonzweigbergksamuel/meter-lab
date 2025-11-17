ac312e4d8139cf477b6b90f9bf930035b8cd0fb6
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-11-17 14:32:56 +0100

    Add DockerFile to backend
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -7,10 +7,12 @@ app.get("/", (c) => {
 	return c.text("Hello Hono!");
 });
 
+const PORT = Number(process.env.PORT) || 3000;
+
 serve(
 	{
 		fetch: app.fetch,
-		port: 3000,
+		port: PORT,
 	},
 	(info) => {
 		console.log(`Server is running on http://localhost:${info.port}`);
