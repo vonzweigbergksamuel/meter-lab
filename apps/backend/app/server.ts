78df7924bb91836268ab0fe2d535f620f78efe5d
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-11-19 18:24:35 +0100

    Add t3 env & update env sample

--- apps/backend/app/server.ts
+++ apps/backend/app/server.ts
@@ -1,5 +1,6 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { env } from "../env.js";
 
 const app = new Hono();
 
@@ -7,12 +8,10 @@ app.get("/", (c) => {
 	return c.text("Hello Hono!");
 });
 
-const PORT = Number(process.env.PORT) || 3000;
-
 serve(
 	{
 		fetch: app.fetch,
-		port: PORT,
+		port: env.PORT,
 	},
 	(info) => {
 		console.log(`Server is running on http://localhost:${info.port}`);
---
+++ apps/backend/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+	server: {
+		PORT: z.number().default(3000),
+	},
+
+	/**
+	 * The prefix that client-side variables must have. This is enforced both at
+	 * a type-level and at runtime.
+	 */
+	clientPrefix: "PUBLIC_",
+	client: {},
+	runtimeEnv: process.env,
+	emptyStringAsUndefined: true,
+});
