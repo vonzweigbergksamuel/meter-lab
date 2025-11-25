a71023be67d820600644815681192f90488d6437
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-11-25 12:49:38 +0100

    Api to call from the server instead
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { createQuery } from "@tanstack/svelte-query";
-	import { orpc } from "$lib/api/client";
+	import { orpc } from "$lib/api/client.server";
 
 	const query = createQuery(() =>
 		orpc.healthCheck.queryOptions({
