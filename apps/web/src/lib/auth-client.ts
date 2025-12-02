1083f4fcc2ae76c8f0bdba05d078056407dbb57e
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-12-02 14:28:06 +0100

    Update web dockerfile, implement fix for local development with docker and public envs

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,7 +1,17 @@
 import { createAuthClient } from "better-auth/svelte"
+import { browser } from "$app/environment";
 import { PUBLIC_AUTH_URL } from "$env/static/public";
 
+// Temporary solution to handle local development on localhost with docker
+// TODO: Implement proper solution for production?
+const isLocalUrl = PUBLIC_AUTH_URL.includes('localhost')
+
+const devUrl = browser 
+    ? PUBLIC_AUTH_URL
+    : PUBLIC_AUTH_URL.replace('localhost', 'host.docker.internal');
+
+const authUrl = isLocalUrl ? devUrl : PUBLIC_AUTH_URL;
+
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: PUBLIC_AUTH_URL
+    baseURL: authUrl
 })
