025b7ff203c014fa5752b84a0760bec9b8b2c4e1
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-12-02 20:50:57 +0100

    Debug AUTH_URL
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -2,17 +2,15 @@ import { createAuthClient } from "better-auth/svelte"
 import { browser } from "$app/environment";
 import { PUBLIC_AUTH_URL } from "$env/static/public";
 
+console.log("PUBLIC_AUTH_URL =", PUBLIC_AUTH_URL);
+
 // Temporary solution to handle local development on localhost with docker
 // TODO: Implement proper solution for production?
 const isLocalUrl = PUBLIC_AUTH_URL.includes('localhost')
 
-// const devUrl = browser 
-//     ? PUBLIC_AUTH_URL
-//     : PUBLIC_AUTH_URL.replace('localhost', 'host.docker.internal');
-
 const devUrl = browser 
     ? PUBLIC_AUTH_URL
-    : PUBLIC_AUTH_URL.replace('localhost', 'auth');
+    : PUBLIC_AUTH_URL.replace('localhost', 'host.docker.internal');
 
 const authUrl = isLocalUrl ? devUrl : PUBLIC_AUTH_URL;
 
