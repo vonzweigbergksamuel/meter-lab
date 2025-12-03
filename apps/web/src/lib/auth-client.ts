d2b6b742794cd9c81692d66d292869497366333a
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-12-03 11:44:34 +0100

    WIP

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,20 +1,24 @@
-import { createAuthClient } from "better-auth/svelte"
 import { browser } from "$app/environment";
 import { PUBLIC_AUTH_URL } from "$env/static/public";
+import { createAuthClient } from "better-auth/svelte";
 
 console.log("PUBLIC_AUTH_URL: ", PUBLIC_AUTH_URL);
 
 const getAuthUrl = () => {
-    if (browser) return PUBLIC_AUTH_URL;
-    if (PUBLIC_AUTH_URL.includes('localhost')) {
-        return PUBLIC_AUTH_URL.replace('localhost', 'host.docker.internal');
-    }
-    return 'http://auth:5090';
+	if (browser) {
+		return PUBLIC_AUTH_URL;
+	}
+
+	if (PUBLIC_AUTH_URL.includes("localhost")) {
+		return PUBLIC_AUTH_URL.replace("localhost", "host.docker.internal");
+	}
+
+	return "http://auth:5090";
 };
 
 const authUrl = getAuthUrl();
 console.log("authUrl: ", authUrl);
 
 export const authClient = createAuthClient({
-    baseURL: authUrl
-})
+	baseURL: authUrl
+});
