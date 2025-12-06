59eb10673fb46e1584d75d12bdac607d6f9b4865
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-12-06 16:08:27 +0100

    Add auth-next service

--- apps/auth/utils/auth.ts
+++ apps/auth/utils/auth.ts
@@ -1,13 +1,13 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { admin, jwt } from "better-auth/plugins";
+import { admin, jwt, openAPI } from "better-auth/plugins";
 import { db } from "../services/db/index.js";
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: "pg",
 	}),
-	plugins: [jwt(), admin()],
+	plugins: [jwt(), admin(), openAPI()],
 	trustedOrigins: [
 		"http://localhost:5173", // Web
 		"http://localhost:5080", // Web
