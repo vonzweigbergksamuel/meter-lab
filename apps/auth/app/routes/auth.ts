9328679ad75b0263676aea303dd00acbe3778e8a
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-12-07 21:04:39 +0100

    Update Cors
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- apps/auth/app/routes/auth.ts
+++ apps/auth/app/routes/auth.ts
@@ -5,7 +5,7 @@ const router = new Hono<{ Bindings: AuthType }>({
 	strict: false,
 });
 
-router.on(["POST", "GET", "OPTIONS"], "/auth/*", (c) => {
+router.on(["POST", "GET"], "/auth/*", (c) => {
 	return auth.handler(c.req.raw);
 });
 
