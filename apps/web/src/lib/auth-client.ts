6990247146d95104a5df7194354ad99356dd028c
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-12-11 16:06:28 +0100

    Update cookie settings
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { PUBLIC_AUTH_URL } from "$env/static/public";
 import { jwtClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/svelte";
 
-const isLocalUrl = PUBLIC_AUTH_URL.includes("localhost");
+// const isLocalUrl = PUBLIC_AUTH_URL.includes("localhost");
 const isExternalUrl = PUBLIC_AUTH_URL.startsWith("https://");
 
 const getAuthUrl = () => {
@@ -11,9 +11,10 @@ const getAuthUrl = () => {
 		return PUBLIC_AUTH_URL;
 	}
 
-	if (isLocalUrl) {
-		return PUBLIC_AUTH_URL;
-	}
+	// osLocalUrl is only needed in local docker
+	// if (isLocalUrl) {
+	// 	return PUBLIC_AUTH_URL;
+	// }
 
 	if (isExternalUrl) {
 		return "http://auth:80";
