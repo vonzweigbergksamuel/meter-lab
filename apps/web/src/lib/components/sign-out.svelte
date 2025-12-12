9332264b18b517a392b6ee2ee40421df58ca1751
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-12-12 09:24:00 +0100

    Remove staging cookie logic from login form and add JWT cookie removal on sign-out.
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- apps/web/src/lib/components/sign-out.svelte
+++ apps/web/src/lib/components/sign-out.svelte
@@ -4,6 +4,11 @@
 
 	const signOut = async () => {
 		await authClient.signOut();
+
+		// Remove JWT cookies
+		document.cookie = "jwt; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+		document.cookie = "jwt-client; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+
 		window.location.href = "/sign-in";
 	};
 </script>
